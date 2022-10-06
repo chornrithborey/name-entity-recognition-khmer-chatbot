75d3d1948c0dccfc2f4cf453de50e801ea19b92a
Sothyrith Hy <sothyrithhy@gmail.com>
2022-10-06 10:44:59 +0700

    exported new sentence variations

--- src/hysothyrith/export_sentences.py
+++ src/hysothyrith/export_sentences.py
@@ -1,10 +1,21 @@
 import os
 import shutil
 
+directory_map = {
+    "raw": "datasets",
+    "segmented": "segment",
+    "tagged": "pos",
+    "encoded": "entity-tag",
+}
+
 
 def main():
-    for file in os.listdir("storage/sentences"):
-        shutil.copy("storage/sentences/{}".format(file), "entity-tag")
+    for key, value in directory_map.items():
+        from_dir = os.path.join("storage/hysothyrith/sentences", key)
+        to_dir = value
+
+        for file in os.listdir(from_dir):
+            shutil.copy(os.path.join(from_dir, file), to_dir)
 
 
 if __name__ == "__main__":
