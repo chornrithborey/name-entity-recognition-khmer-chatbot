fe8c3b298f448949b385794222abe368e9c44dcb
Sothyrith Hy <sothyrithhy@gmail.com>
2022-10-13 12:09:12 +0700

    add test to check for invalid entity tags

---
+++ tests/test_tags_are_valid.py
@@ -0,0 +1,68 @@
+import os
+
+
+def all_sentences():
+    root_dir = "entity-tag"
+    files = os.listdir(root_dir)
+
+    for file in files:
+        with open(os.path.join(root_dir, file), "r") as f:
+            f.readline()
+            for line in f:
+                yield line.strip().split(",")[0].strip()
+
+    return files
+
+
+def get_all_entity_tags():
+    entity_tags = set()
+    for sentence in all_sentences():
+        for word in sentence.split():
+            segments = word.split("/")
+
+            if len(segments) == 2:
+                entity_tags.add(segments[1])
+            elif len(segments) > 2:
+                raise ValueError("Invalid word: {}".format(word))
+
+    return entity_tags
+
+
+def test_tags_are_valid():
+    valid_tags = set(
+        [
+            "B-ROOM",
+            "I-ROOM",
+            "B-BUILD",
+            "I-BUILD",
+            "B-PER",
+            "I-PER",
+            "B-FLOOR",
+            "I-FLOOR",
+            "B-ORD",
+            "I-ORD",
+            "B-CAR",
+            "I-CAR",
+            "B-DATE",
+            "I-DATE",
+            "B-TIME",
+            "I-TIME",
+            "B-MONEY",
+            "I-MONEY",
+            "B-EVE",
+            "I-EVE",
+            "B-ANN",
+            "I-ANN",
+            "B-GPE",
+            "I-GPE",
+            "B-LOC",
+            "I-LOC",
+            "B-ORG",
+            "I-ORG",
+            "B-MAJOR",
+            "I-MAJOR",
+        ]
+    )
+
+    for tag in get_all_entity_tags():
+        assert tag in valid_tags, "Invalid tag: {}".format(tag)
