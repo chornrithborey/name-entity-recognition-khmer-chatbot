f83ed768a6c34059a5e98cb285ae6c5d48c3d635
Sothyrith Hy <sothyrithhy@gmail.com>
2022-10-13 13:26:42 +0700

    remove failing TESTONLY.csv file

--- src/generate_detailed_stats.py
+++ src/generate_detailed_stats.py
@@ -64,33 +64,13 @@ def collect_entities(segments: list[dict]) -> list[dict]:
     return entities
 
 
-def group_entities_into(dictionary: dict, segments: list[dict]):
-    for segment in [segment for segment in segments if segment["entity"]]:
-        entity_tag = segment["entity"]
-        entity_type = entity_tag.split("-")[1]
-
-        if not entity_type in dictionary:
-            dictionary[entity_type] = {}
-
-        if not entity_tag in dictionary[entity_type]:
-            dictionary[entity_type][entity_tag] = {}
-
-        if not segment["text"] in dictionary[entity_type][entity_tag]:
-            dictionary[entity_type][entity_tag][segment["text"]] = 0
-
-        dictionary[entity_type][entity_tag][segment["text"]] += 1
-
-    return dictionary
-
-
 def collect_stats(directory: str) -> tuple:
     files = os.listdir(directory)
 
     words_count = {}
     entities_count = {}
-    # entity_types_count = {}
+    entity_types_count = {}
     max_length = 0
-    entity_groups = {}
 
     for file in files:
         with open(directory + "/" + file, "r") as f:
@@ -104,15 +84,13 @@ def collect_stats(directory: str) -> tuple:
                 for word in segments:
                     count_into(words_count, word["text"])
 
-                group_entities_into(entity_groups, segments)
+                for entity in collect_entities(segments):
+                    count_into(
+                        entities_count, entity["value"] + ":" + entity["type_code"]
+                    )
+                    count_into(entity_types_count, entity["type_code"])
 
-                # for entity in collect_entities(segments):
-                #     count_into(
-                #         entities_count, entity["value"] + ":" + entity["type_code"]
-                #     )
-                #     count_into(entity_types_count, entity["type_code"])
-
-    return words_count, entities_count, entity_groups, max_length
+    return words_count, entities_count, entity_types_count, max_length
 
 
 def main():
