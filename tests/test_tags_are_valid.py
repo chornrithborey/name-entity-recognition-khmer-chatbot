a22c45b31cffbda18a51bb6e4559cc09368b7285
Sothyrith Hy <sothyrithhy@gmail.com>
2022-10-13 13:49:34 +0700

    add test to prevent empty entity tags

--- tests/test_tags_are_valid.py
+++ tests/test_tags_are_valid.py
@@ -20,10 +20,11 @@ def get_all_entity_tags():
         for word in sentence.split():
             segments = word.split("/")
 
+            assert len(segments) < 3, "Invalid word: {}".format(word)
+
             if len(segments) == 2:
+                assert segments[0] != "", "Empty tag: {}".format(word)
                 entity_tags.add(segments[1])
-            elif len(segments) > 2:
-                raise ValueError("Invalid word: {}".format(word))
 
     return entity_tags
 
